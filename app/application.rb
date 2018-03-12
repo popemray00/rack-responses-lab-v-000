083f105bf99549265fdca625d845c021a65f94b5
Michael Pope <popemray1986@gmail.com>
2018-03-12 03:57:54 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    if datetime.now < 12:00
+    if datetime.now < 12
       resp.write "Good Morning"
     else
       resp.write "Good Afternoon"
